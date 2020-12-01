ea2bce911532c169bcefc42592e630f57dd81dd2
kblauhut <koljablauhut@gmail.com>
2020-12-01 09:52:44 +0100

    -Moved server into its own repository, also fixed some issues

--- extension/src/components/App/App.js
+++ extension/src/components/App/App.js
@@ -42,7 +42,8 @@ export default class App extends React.Component {
                 this.Authentication.setToken(auth.token, auth.userId)
                 if (!this.state.finishedLoading) {
                     // if the component hasn't finished loading (as in we've not set up after getting a token), let's set it up now.
-
+                    console.log(auth.token)
+                    console.log(auth.userId)
                     // now we've done the setup for the component, let's set the state to true to force a rerender with the correct data.
                     this.setState(() => {
                         return { finishedLoading: true }
